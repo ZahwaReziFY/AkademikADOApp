374de9c14129884394ede40332ea1b4e6d827635
ZahwaReziFY <zahwarezi416@gmail.com>
2026-03-10 13:14:58 +0700

    feat: create AkademikADOApp namespace

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -11,7 +11,7 @@ using System.Data.SqlClient;
 
 namespace AkademikADOApp
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form 
     {
         //Connection string
         string connString = 
