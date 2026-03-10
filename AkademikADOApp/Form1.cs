06f9958f9fb5d92e2fe0490e6f008900184355a6
ZahwaReziFY <zahwarezi416@gmail.com>
2026-03-10 13:24:19 +0700

    membuat pesan koneksi ke database

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -31,7 +31,7 @@ namespace AkademikADOApp
                 conn = new SqlConnection(connString); //feat: create SqlConnection instance
                 conn.Open();
                 lblStatus.Text = "Status : Database Connected"; //membuat status database terkoneksi
-                MessageBox.Show("Koneksi ke database berhasil!");
+                MessageBox.Show("Koneksi ke database berhasil!"); //membuat pesan koneksi ke database berhasil
             }
             catch (Exception ex)
             {
